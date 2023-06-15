d1f00eee9a690d65b573f7ad5f80941cb2e3cadd
Eva Reitzel <evareitzel@gmail.com>
2023-06-15 04:15:50 -0400

    commit tree cff2dc43ff680e439243885db64cc87403c2be8d

---
+++ app/models/show.rb
@@ -0,0 +1,78 @@
+# binding.pry
+
+
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+      Show.maximum(:rating)
+    end
+
+    def self.most_popular_show
+      Show.where(rating: highest_rating).first # [0]
+    end
+
+    def self.lowest_rating
+      self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+      Show.where(rating: lowest_rating).first
+    end
+
+    def self.ratings_sum
+      Show.sum(:rating)
+    end
+
+    def self.popular_shows
+      # where
+      Show.where("rating >= ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+      Show.order(:name)
+    end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def self.highest_rating
+#   self.maximum(:rating)
+# end
+
+# def self.most_popular_show
+#   self.where("rating = ?", self.highest_rating).first
+# end
+
+# def self.lowest_rating
+#   self.minimum(:rating)
+# end
+
+# def self.least_popular_show
+#   self.where("rating = ?", self.lowest_rating).first
+# end
+
+# def self.ratings_sum
+#   self.sum(:rating)
+# end
+
+# def self.popular_shows
+#   self.where("rating >= ?", 5)
+# end
+
+# def self.shows_by_alphabetical_order
+#   self.order(:name)
+# end
